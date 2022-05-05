3e59c6bf3a817b69acc796db7d6c9dc210b69d23
woojou <532651115@qq.com>
2022-05-05 17:15:08 +0800

    Initial commit

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,20 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    //这里的userId是为了后面个人用户查看自己发布的帖子做准备
+    //当userId=0的时候这个条件是忽略的（用到了动态查询语句）
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
