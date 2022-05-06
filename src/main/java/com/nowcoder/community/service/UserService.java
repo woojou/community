650b4a684ced0daad078a2d158cdc56a8c93fb8c
woojou <532651115@qq.com>
2022-05-06 18:19:20 +0800

    注册以及邮件激活功能

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,15 +2,110 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.MailClient;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
     @Autowired
     private UserMapper userMapper;
 
+    @Resource
+    private MailClient mailClient;
+
+    @Resource
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+
     public User findUserById(int id) {
         return userMapper.selectById(id);
+    }
+
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
+
+        if (user == null) {
+            throw new IllegalArgumentException("参数不能为空");
+        }
+
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "账号不能为空！");
+            return map;
+        }
+
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "密码不能为空！");
+            return map;
+        }
+
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空！");
+            return map;
+        }
+
+        //验证账号
+        User user1 = userMapper.selectByName(user.getUsername());
+        if (user1 != null) {
+            map.put("usernameMsg", "该账号已存在！");
+            return map;
+        }
+
+        //验证邮箱
+        user1 = userMapper.selectByEmail(user.getEmail());
+        if (user1 != null) {
+            map.put("emailMsg", "该邮箱已被注册！");
+            return map;
+        }
+
+        //注册用户
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
+        user.setType(0);//普通用户
+        user.setStatus(0);//未激活
+        user.setActivationCode(CommunityUtil.generateUUID());//设置激活码
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user); //加入库内
+
+        //给用户发送激活邮件
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
+
+        return map;
+    }
+
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if(user.getStatus() == 1) {
+            return ACTIVATION_REPEAT;
+        } else if(user.getActivationCode().equals(code)) {
+            userMapper.updateStatus(userId, 1);
+            return ACTIVATION_SUCCESS;
+        } else {
+            return ACTIVATION_FAILURE;
+        }
     }
 }
---
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community.util;
+
+import com.mysql.jdbc.StringUtils;
+import org.springframework.util.DigestUtils;
+
+import java.util.UUID;
+
+public class CommunityUtil {
+
+    //生成随机字符串
+    public static String generateUUID() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    //MD5加密(对用户密码加密）
+    //只能加密不能解密，相同字符串加密每次都会得到相同的密码
+    //利用salt（一个随机字符串）,将密码和salt拼接后传入key,之后再加密
+    public static String md5(String key) {
+        if(StringUtils.isNullOrEmpty(key)) {
+            return null;
+        }
+        return DigestUtils.md5DigestAsHex(key.getBytes());
+    }
+
+}
